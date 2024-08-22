0e415d867bc8448c2d2fad6012b0fc89d1ded620
intagorn <30479054+intagorn@users.noreply.github.com>
2024-08-22 09:03:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
-# YOUR CODE HERE
-x = float(input())
-y = float(input())
 
-if x > 0 and y > 0 :
-    print('Q1')
-elif x < 0 and y > 0 :
-    print('Q2')
-elif x < 0 and y < 0 :
-    print('Q3')
-elif x > 0 and y < 0 :
-    print('Q4')
-else:
-    print('Undetermined quadrant')
 
